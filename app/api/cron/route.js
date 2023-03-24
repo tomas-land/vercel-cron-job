d5c8f75ed0e37cf09d7f2f1334364e71e455b5e2
tomas <72792707+tomas-land@users.noreply.github.com>
2023-03-25 01:02:25 +0200

    init

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,9 +1,9 @@
-export default function handler(req, res) {
-  try {
 
+export async function GET(request) {
+  try {
+  
     res.status(200).json({ name: 'endpoint success' })
   } catch (error){
     res.status(500).json({ name: 'John Doe' })
   }
-
 }
