b0a7f71cc7ca9a8cb491326f1cfa566562ff581c
tomas <72792707+tomas-land@users.noreply.github.com>
2023-03-25 01:13:43 +0200

    init

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,5 +1,18 @@
 
 export async function GET(request) {
-  return new Response('endpoint: cron-vb\app\api\cron\route.js')
+
+  var settings = {
+    "url": `https://savitarna.smsbiuras.lt/api?uid=2874&apikey=${process.env.SMS_API_KEY}&from=VB-studija&to=37067532865&message=sms siuncia`,
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+        "Accept": "application/json"
+    },
+};
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
+  return new Response({success: true})
 
 }
