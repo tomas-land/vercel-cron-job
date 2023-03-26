47ea5cdef4ba684674fd01cc66a6c1fa71896d98
tomas <72792707+tomas-land@users.noreply.github.com>
2023-03-27 00:09:14 +0300

    update

--- app/page.js
+++ app/page.js
@@ -1,21 +1,14 @@
 import styles from "./page.module.css";
 import prisma from "../lib/prisma";
 
-export async function GET(request) {
-  const visits = await prisma.visits.findMany();
-  return new Response(JSON.stringify({visits}), {
-    headers: {
-      "Content-Type": "application/json",
-      },
-      });
-}
+
 
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
-ff
+ffd
     </main>
   );
 }
